325c606b00b46ccd0704c2d618603bc752f539c9
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-29 16:40:25 -0500

    style: new higher qualirt battle-matt image

--- src/cardspage/cardspage.styles.js
+++ src/cardspage/cardspage.styles.js
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles'
+import img from '../../public/bgmatt.jpg'
 
 export const useStyles = makeStyles(theme => ({
          root: {
-           backgroundImage:
-             "url(' https://i.pinimg.com/originals/db/ae/df/dbaedf15b9f61d0fc040ad15bc7483e2.jpg')",
+           backgroundImage: `url(${img})`,
            backgroundAttachment: "fixed",
            backgroundRepeat: "no-repeat",
            backgroundSize: "100%",
--- src/searchform/searchform.styles.js
+++ src/searchform/searchform.styles.js
@@ -1,38 +1,37 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles(theme => ({
-    root: {
+         root: {},
 
-    },
+         searchButton: {
+           height: 65,
+           borderRadius: 10
+         },
 
-    searchButton: {
-        height: 65,
-        borderRadius: 10,
-    },
+         searchForm: {
+           margin: 20
+         },
 
-    searchForm: {
-        margin: 20
-    },
+         textField: {
+           marginRight: 20,
+           width: 500,
+           borderRadius: 10,
 
-    textField: {
-        marginRight: 20,
-        width: 500,
-        borderRadius: 10,
+           " & div": {
+             fontFamily: "Barlow",
+             color: "white",
+             fontSize: "1.6em",
+             borderRadius: 10
+           },
+           "& label": {
+             fontFamily: "Barlow",
+             color: "white",
+             fontSize: "1.1em"
+           },
+           "& fieldset": {
+             fontFamily: "Barlow",
+             color: "white"
+           },
 
-        " & div": {
-            fontFamily: "Barlow",
-            color: "white",
-            fontSize: "1.6em",
-            borderRadius: 10
-        },
-        "& label": {
-            fontFamily: "Barlow",
-            color: "white",
-            fontSize: "1.1em"
-        },
-        "& fieldset": {
-            fontFamily: "Barlow",
-            color: "white"
-        }
-    }
-}));
+         }
+       }));
