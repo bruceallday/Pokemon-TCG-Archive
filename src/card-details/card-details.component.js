0c2a028be60b28a5486f82bc478ca10e9f239b23
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-30 13:57:31 -0500

    style: card details, details container

--- src/card-details/card-details.component.js
+++ src/card-details/card-details.component.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useStyles from './card-details.styles'
 
-
 const CardDetails = () => {
   const { name, cardID } = useParams()
 
@@ -32,10 +31,27 @@ const CardDetails = () => {
 
   return (
     <div>
-      {data ? <img className={classes.cardImage} src={data.cards[0].imageUrlHiRes} alt="hi resolution" /> : <p />}
+      {data ? (
+        <div className={classes.detailsContainer}>
+          <img
+            className={classes.cardImage}
+            src={data.cards[0].imageUrlHiRes}
+            alt="hi resolution"
+          />
+          <div className={classes.cardDetails}>
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+          </div>
+        </div>
+      ) : (
+        <p />
+      )}
+
       {data ? console.log(data.cards) : <p />}
     </div>
-  )
+  );
 }
 
 export default CardDetails
--- src/card-details/card-details.styles.js
+++ src/card-details/card-details.styles.js
@@ -10,6 +10,16 @@ const useStyles = makeStyles(() => ({
     height: 740,
   },
 
+  detailsContainer: {
+    padding: '160 0 0 100',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  cardDetails: {
+    width: '40vw',
+    backgroundColor: 'red',
+  },
 }))
 
 export default useStyles
