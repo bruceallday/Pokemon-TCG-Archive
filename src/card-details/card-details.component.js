72e42f15f9b7217ed5ad4c012bdb755b6b3eca00
Bruce Pouncey <bruce.pouncey@yahoo.com>
2020-01-30 11:08:33 -0500

    feat: fetch card detail data for <CardsDeatails/> page from  card id params

--- src/card-details/card-details.component.js
+++ src/card-details/card-details.component.js
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+
 
 const CardDetails = () => {
   const { name, cardID } = useParams()
   console.log('IN CARD DETAILS')
   console.log(name, cardID)
 
+  const [data, setData] = useState(null)
+
+  const getData = async (card) => {
+    setData(null)
+
+    const result = await fetch(`https://api.pokemontcg.io/v1/cards?id=${card}`)
+
+    const formattedData = await result.json()
+
+    if (formattedData.error) {
+      console.log(formattedData.error)
+    } else {
+      setData(formattedData)
+      console.log(data)
+    }
+  }
+
+  useEffect(() => {
+    getData(cardID)
+  }, [])
+
   return (
-    <div>{name + cardID}</div>
+    <div>
+      { data ? console.log(data) : <p />}
+    </div>
   )
 }
 
